449d93d9e1f105bd0913292bb6c7eb1eaec3e55c
Vincenzo Acinapura <vin.aci@gmail.com>
2013-10-19 13:21:55 +0200

    Sites controller and devise views added.

---
+++ app/controllers/sites_controller.rb
@@ -0,0 +1,24 @@
+class SitesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Hermes::Application.routes.draw do
   devise_for :users
+
+  resources :sites
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
