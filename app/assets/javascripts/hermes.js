e2b35edb764acd904cc57f1fd60e9a1a3373358c
Vincenzo Acinapura <vin.aci@gmail.com>
2013-10-19 14:54:41 +0200

    Draft JavaScript that should be loaded by the target site.

---
+++ app/assets/javascripts/hermes.js
@@ -0,0 +1,34 @@
+(function() {
+  var jQuery;
+
+  if (window.jQuery === undefined || window.jQuery.fn.jquery !== '1.10.2') {
+    var script_tag = document.createElement('script');
+    script_tag.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js");
+
+    if (script_tag.readyState) {
+      script_tag.onreadystatechange = function () {
+        if (this.readyState == 'complete' || this.readyState == 'loaded') {
+          scriptLoadHandler();
+        }
+      };
+    } else {
+      script_tag.onload = scriptLoadHandler;
+    }
+    (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
+  } else {
+    jQuery = window.jQuery;
+    main();
+  }
+
+  function scriptLoadHandler() {
+    jQuery = window.jQuery.noConflict(true);
+    main();
+  }
+
+  function main() {
+    jQuery(document).ready(function($) {
+      // Hermes code here.
+      alert($.fn.jquery);
+    });
+  }
+})();
