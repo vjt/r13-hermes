7e4f3a146c22bbaedea07de17e7f77558076e466
Marcello Barnaba <vjt@openssl.it>
2013-10-19 10:15:05 +0200

    Create Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hermes::Application.config.secret_key_base = '9c09c673327cdfcb7d41cc02a626152b975747988abadfd5298f51f2cf4813be0636a4eeece10409cd5d3848d18217d30d04cabf3036888682024494daabdc61'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hermes::Application.config.session_store :cookie_store, key: '_hermes_session'
