556cca8600dfe11e0b0f5ded9833c0b99d9c8a28
Marcello Barnaba <vjt@openssl.it>
2013-10-20 13:28:44 +0200

    Whitespace

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -2,6 +2,6 @@ $(function () {
   $("a[rel~=popover], .has-popover").popover();
   $("a[rel~=tooltip], .has-tooltip").tooltip();
   $("input.datepicker").datepicker({
-                                      format: "yyyy-mm-dd"
+    format: "yyyy-mm-dd"
   });
 });
