bef56c8f9fbafb201151d802be1e1d81ee75c33a
Amedeo Paglione <amedeo.paglione@gmail.com>
2013-10-20 04:33:32 +0200

    Better dates

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'therubyracer', platforms: :ruby
 gem 'less-rails'
 gem 'twitter-bootstrap-rails'
+gem 'bootstrap-datepicker-rails'
 
 group :doc do
   gem 'sdoc', require: false
--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -1,4 +1,7 @@
 $(function () {
   $("a[rel~=popover], .has-popover").popover();
   $("a[rel~=tooltip], .has-tooltip").tooltip();
+  $("input.datepicker").datepicker({
+                                      format: "yyyy-mm-dd"
+  });
 });
