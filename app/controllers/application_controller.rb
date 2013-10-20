a8fea3230b2393ae058818310631a1d1980f488c
Marcello Barnaba <vjt@openssl.it>
2013-10-21 01:38:43 +0200

    Remove leftover

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  before_filter :find_sites, :if => :current_user
-
-  def find_sites
-  end
 end
