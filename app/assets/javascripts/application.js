f0aff9dc11b20cd2c131d133a5933acc450cb2f3
Vincenzo Acinapura <vin.aci@gmail.com>
2013-10-20 20:15:16 +0200

    Started working on sortable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.ui.sortable
+//= require jquery.ui.draggable
 //= require jquery.ui.effect-highlight
 //= require twitter/bootstrap
 //= require bootstrap
